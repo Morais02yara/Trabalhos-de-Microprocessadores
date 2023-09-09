822fc686898ca6ef88f085ad664c007050dfbc4f
Yara Morais <107282272+Morais02yara@users.noreply.github.com>
2023-09-09 20:30:50 -0300

    Parte 02 do trabalho na linguagem Assembly
    Exercício 04: Geração de forma de onda por meio do número da matricula
    
    *Auxilio da ferramenta Proteus para geração das formas de onda

--- main.asm
+++ main.asm
@@ -1,48 +1,81 @@
-include "p18f4550.inc"
-    
-    VARIAVEIS UDATA_ACS 0
- 
-	; Variavel que conterá o número de 8 bits
-	X RES 1
- 
-	; Variável que irá contar a quantidade de 1's
-	CONT RES 1
- 
-RES_VECT CODE 0x0000 
-    GOTO START 
-    
-MAIN_PROG CODE
+#include "p18f4550.inc"
 
- START
-    ; Definir o valor de X
-    MOVLW b'00101101'   ; W recebe um o número binário de 8 bits
-    MOVWF X  ; A variável X recebe W
+CONFIG FOSC = HS ; Oscilador externo em uso
+CONFIG WDT = OFF ; Desativar Watchdog Timer
+CONFIG LVP = OFF ; Desativar programação de baixa tensão
+
+ORG 0x0000
+GOTO START
     
-    ; Inicializa o contador em 0
-    MOVLW .0
+ORG 0x0008
+RETURN
     
-    ; Em cada posição do número binário o programa verifica se tem 1
-    ; Quando encontra 0, pula uma linha. Se encontrar 1, continua na próxima linha.
-    ; A cada bit 1 encontrado é adicionado 1 a variável W
+START
+    ; Configurar as portas RC0, RC1 e RC2 como saídas
+    BCF TRISC, 0
+    BCF TRISC, 1
+    BCF TRISC, 2
     
-    BTFSC X,7
-    ADDLW .1
-    BTFSC X,6
-    ADDLW .1
-    BTFSC X,5
-    ADDLW .1
-    BTFSC X,4
-    ADDLW .1
-    BTFSC X,3
-    ADDLW .1
-    BTFSC X,2
-    ADDLW .1
-    BTFSC X,1
-    ADDLW .1
-    BTFSC X,0
-    ADDLW .1
+    ; Loop principal
+LOOP:
+    ; C = 0 S = 1
     
-    ; O resultado de W é copiado no contador 
-    MOVWF CONT
+    BSF PORTC, 0 ; Defini a saida RCO como 1
+    BCF PORTC, 1 ; Defini a saida RCO como 0
+    BCF PORTC, 2 ; Defini a saida RCO como 0
+    CALL Delay_800us
     
-    END
+    BCF PORTC, 0 ; Defini a saida RCO como 0
+    BSF PORTC, 1 ; Defini a saida RCO como 1
+    BCF PORTC, 2 ; Defini a saida RCO como 0
+    CALL Delay_800us
+    
+    BCF PORTC, 0 ; Defini a saida RCO como 0
+    BCF PORTC, 1 ; Defini a saida RCO como 0
+    BSF PORTC, 2 ; Defini a saida RCO como 1
+    CALL Delay_800us
+    
+    BSF PORTC, 0 ; Defini a saida RCO como 1
+    BSF PORTC, 1 ; Defini a saida RCO como 1
+    BCF PORTC, 2 ; Defini a saida RCO como 0
+    CALL Delay_800us
+    
+    BCF PORTC, 0 ; Defini a saida RCO como 0
+    BSF PORTC, 1 ; Defini a saida RCO como 1
+    BSF PORTC, 2 ; Defini a saida RCO como 1
+    CALL Delay_800us
+    
+    BSF PORTC, 0 ; Defini a saida RCO como 1
+    BCF PORTC, 1 ; Defini a saida RCO como 0
+    BSF PORTC, 2 ; Defini a saida RCO como 1
+    CALL Delay_800us
+    
+    BSF PORTC, 0 ; Defini a saida RCO como 1
+    BCF PORTC, 1 ; Defini a saida RCO como 0
+    BCF PORTC, 2 ; Defini a saida RCO como 0
+    CALL Delay_800us
+    
+    BCF PORTC, 0 ; Defini a saida RCO como 0
+    BSF PORTC, 1 ; Defini a saida RCO como 1
+    BCF PORTC, 2 ; Defini a saida RCO como 0
+    CALL Delay_800us
+    
+    GOTO LOOP
+    
+Delay_800us:
+    MOVLW 0x0310 ; Valor inicial para um atraso de 800us em hexadecimal
+    MOVWF DELAY_COUNT ; Armazenar o valor inicial em DELAY_COUNT
+    
+WAIT_DELAY:
+    NOP ; Atraso de 1 ciclo de instrução
+    DECFSZ DELAY_COUNT, 1 ; Decrementar DELAY_COUNT e verificar se é 0
+    GOTO WAIT_DELAY ; Aguardar até que DELAY_COUNT atinja 0
+    
+    RETURN
+    
+DELAY_COUNT RES 1 ; Variável para armazenar o contador de atraso
+ 
+END
+    
+    
+    
